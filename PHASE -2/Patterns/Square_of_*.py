657b0b34c233c53a44aad848d7e3da13918428d1
Aburoobha  A <aburoobhastudy@gmail.com>
2025-09-20 22:03:21 +0530

    Create Square_of_*.py

---
+++ PHASE -2/Patterns/Square_of_*.py
@@ -0,0 +1,22 @@
+#Question : Print a square of * of size n.
+
+n = int(input())
+for i in range(n):
+    print("*"*n)
+
+'''
+output:
+****
+****
+****
+****
+'''
+
+'''I have learned here about 
+The underscore _
+In Python, _ is often used as a throwaway variable name, meaning:
+"I need a variable here syntactically, but I don’t actually care about its value."
+
+      for i in range(5) ---->  for _ in range(5)
+'''
+
