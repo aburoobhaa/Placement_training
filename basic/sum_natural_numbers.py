16671491e9d8e7d6880194cf4773fd44ed2d72a1
Aburoobha  A <aburoobhastudy@gmail.com>
2025-09-20 18:55:12 +0530

    Create sum_natural_numbers.py

---
+++ basic/sum_natural_numbers.py
@@ -0,0 +1,18 @@
+# question Print the sum of the first N natural numbers.
+
+n = int(input())
+tot = 0
+for i in range(1,n+1):
+    tot +=i
+print(tot)
+
+'''
+NOTE: In placements, sometimes they also expect you to know the formula shortcut:
+
+Sum of first N numbers =𝑛×(𝑛+1)/ 2
+'''
+
+n == int(input())
+print(n*(n+1)//2)
+
+	​
