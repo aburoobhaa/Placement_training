8292360d40119c9f6367ecd0496bc288ac32a076
Aburoobha  A <aburoobhastudy@gmail.com>
2025-09-20 22:22:06 +0530

    Create inverted triangle.py

---
+++ PHASE -2/Patterns/inverted triangle.py
@@ -0,0 +1,17 @@
+#question: Print an inverted triangle of *.
+
+n = int(input())
+for i in range(n,0,-1):
+    print("*"*i)
+
+'''
+output:
+*****
+****
+***
+**
+*
+'''
+
+'''
+NOTE: IT IS ALL IN LOOP. 
